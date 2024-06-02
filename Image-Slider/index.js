a35d014db4f75915a862aca1751151d631fc14ce
Rakesh-dey-007 <deyrakesh.official.2004@gmail.com>
2024-06-02 09:55:44 +0530

    Add a New Project : Animated Image Slider

---
+++ Image-Slider/index.js
@@ -0,0 +1,9 @@
+
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+
+next.addEventListener('click', function () {
+    let items = document.querySelectorAll(".item");
+    document.querySelector(".slide").appendChild(items[0]);
+})
+
